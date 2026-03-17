85b1c99fc0bb19cc3902ea0cb4bcaeeb5c34535e
Ticket <dynastie_biopic.5l@icloud.com>
2026-03-17 22:15:29 +0100

    Adding workfow to automate deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 export default defineConfig({
 	root: path.resolve(__dirname),
 	build: {
-		outDir: path.resolve(__dirname, "./upload"),
+		outDir: path.resolve(__dirname, "./public"),
 		emptyOutDir: true,
 		sourcemap: true,
 		minify: "esbuild",
